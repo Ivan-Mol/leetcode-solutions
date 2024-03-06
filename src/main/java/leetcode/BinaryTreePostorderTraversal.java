02342c813786a2e554224365d387faf809ec2b7e
Ivan-Mol <abyss.devotee@gmail.com>
2024-03-06 18:10:13 +0100

    12

---
+++ src/main/java/leetcode/BinaryTreePostorderTraversal.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface BinaryTreePostorderTraversal {
+    static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ints = new ArrayList<>();
+        addNums(root,ints);
+        return ints;
+    }
+    static void addNums(TreeNode root, List<Integer> ints){
+        if(root!=null){
+            addNums(root.left,ints);
+            addNums(root.right,ints);
+            ints.add(root.val);
+        }
+    }
+}
